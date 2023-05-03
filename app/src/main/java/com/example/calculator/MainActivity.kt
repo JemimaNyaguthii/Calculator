ec254c4dc927f62ab4c536d7e849a4ddc405d0cb
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-05-03 17:04:03 +0300

    calculator

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.calculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    lateinit var et: EditText
+    lateinit var btn1: Button
+    lateinit var btn2: Button
+    lateinit var btn3: Button
+    lateinit var btn4: Button
+    lateinit var btn5: Button
+    lateinit var btn6: Button
+    lateinit var btn7: Button
+    lateinit var btn8: Button
+    lateinit var btn9: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }}
